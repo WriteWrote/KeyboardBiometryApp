e9b63838eb82924ebccc000589a99ed68e746823
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2022-11-20 12:28:19 +0300

    safe commit

--- Lab1KeyboasrdBiometry/RegisterForm.cs
+++ Lab1KeyboasrdBiometry/RegisterForm.cs
@@ -1,10 +1,15 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Lab1KeyboasrdBiometry
 {
     public partial class RegisterForm : Form
     {
+        private String USERLIST = "userlist.txt";
+        private List<String> names;
         public RegisterForm()
         {
             InitializeComponent();
@@ -19,6 +24,26 @@ namespace Lab1KeyboasrdBiometry
         private void button_Create_Click(object sender, EventArgs e)
         {
             throw new System.NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+            names = new List<string>();
+            using (StreamReader reader = new StreamReader(USERLIST))
+            {
+                String text = reader.ReadToEnd();
+                names = text.Split('\n').ToList();
+            }
+
+            foreach (var name in names)
+            {
+                comboBox_users.Items.Add(name);
+            }
         }
     }
 }
