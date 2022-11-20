bc7657f1a4905f69e21724ce2f373db2f07adf3e
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2022-11-20 12:46:52 +0300

    stats-saving system

--- Lab1KeyboasrdBiometry/RegisterForm.cs
+++ Lab1KeyboasrdBiometry/RegisterForm.cs
@@ -10,9 +10,18 @@ namespace Lab1KeyboasrdBiometry
     {
         private String USERLIST = "userlist.txt";
         private List<String> names;
+        private List<String> FILES;
         public RegisterForm()
         {
             InitializeComponent();
+            
+            FILES.Add("currentKeyPhrase");
+            FILES.Add("phr1KDownLog");
+            FILES.Add("phr2KDownLog");
+            FILES.Add("phr1KUpLog");
+            FILES.Add("phr2KUpLog");
+            FILES.Add("phrase1Stats");
+            FILES.Add("phrase2Stats");
         }
 
         private void button_Enter_Click(object sender, EventArgs e)
@@ -23,12 +32,28 @@ namespace Lab1KeyboasrdBiometry
 
         private void button_Create_Click(object sender, EventArgs e)
         {
+            User newUser = new User(textBox_name.Text.Trim(),
+                textBox_password.Text,
+                0,
+                0,
+                0);
             
-        }
+            // creating files w/ stats for user
+            Directory.CreateDirectory(newUser.Name);
+            string path = @"c:\temp\MyTest.txt";
+            if (!File.Exists("\\" + newUser.Name + ""))
+            {
+                // Create a file to write to.
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("And");
+                    sw.WriteLine("Welcome");
+                }
+            }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            throw new System.NotImplementedException();
+            // adding username to users
+
         }
 
         private void RegisterForm_Load(object sender, EventArgs e)
@@ -44,6 +69,9 @@ namespace Lab1KeyboasrdBiometry
             {
                 comboBox_users.Items.Add(name);
             }
+            
+            
+            // todo: добавить считывание всех файлов, начинающихся с user... и вытаскивание оттуда средних параметров
         }
 
         private void comboBox_users_SelectedIndexChanged(object sender, EventArgs e)
